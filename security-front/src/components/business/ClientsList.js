8f2d7b7e71959c9dc02048c20cdcc79971784a30
Aksont <aastanivuk@gmail.com>
2023-06-16 13:25:13 +0200

    front clients search

--- security-front/src/components/business/ClientsList.js
+++ security-front/src/components/business/ClientsList.js
@@ -1,28 +1,40 @@
 import React, { useCallback } from 'react';
 import '../../assets/styles/business.css';
 import {useEffect, useState} from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 import { getCertificates } from '../../services/api/CertificatesApi';
 import ListedRequest from './ListedRequest';
 import ListedCertificate from './ListedCertificate';
 import { useNavigate } from 'react-router';
 import ListedObject from './ListedObject';
-import { getAllClients } from '../../services/api/UserApi';
+import { getAllClients, postClientFilterRequest, sendFilterClientsRequest } from '../../services/api/UserApi';
 import { getRole } from '../../services/utils/AuthService';
 import ListedClient from './ListedClient';
+import LabeledInput from '../forms/LabeledInput';
+import { checkLettersInput, checkEmailInput } from '../../services/utils/InputValidation';
 
 export default function ClientsList(){
 
     const [clients, setClients] = useState();
     const [listedClients, setListedClients] = useState([]);
+    const [filterName, setFilterName] = useState("");
+    const [filterSurname, setFilterSurname] = useState("");
+    const [filterEmail, setFilterEmail] = useState("");
+    const [filterRole, setFilterRole] = useState("");
 
     useEffect(() => {
-        // TODO get all clients
-        getAllClients().then(
-            (response) => {
-                setClients(!!response ? response.data : []);
-            }
-        )
+        const filterParams = JSON.parse(sessionStorage.getItem("filterParams"));
+
+        if (!filterParams){
+            getAllClients().then(
+                (response) => {
+                    setClients(!!response ? response.data : []);
+                }
+            )
+        } else {
+            // TODO filter users from back by params
+            // TODO set filter params into name, surname, email, role
+        }
     }, [])
 
     useEffect(() => {
@@ -33,12 +45,82 @@ export default function ClientsList(){
         }
     }, [clients])
 
+    
+  
+    const validateInput = () => {
+        let valid = (checkLettersInput(filterName) || filterName.length === 0 ) && 
+                    (checkLettersInput(filterSurname) || filterSurname.length === 0 ) && 
+                    (checkEmailInput(filterEmail) || filterEmail.length === 0 ) && 
+                    (filterRole.length === 0 || filterRole.toLowerCase() === "renter" || filterRole.toLowerCase() === "owner" ) 
+                    ;
+  
+        return valid;
+    }
+
+    const resetButtonPressed = (e) => {
+        e.preventDefault();
+        getAllClients().then(
+            (response) => {
+                setClients(!!response ? response.data : []);
+            }
+        );
+        sessionStorage.removeItem("filterParams");
+    }
+
+    const filterButtonPressed = (e) => {
+        if (validateInput()) {
+            postFilterClientsRequest(e);
+        } else {
+          console.log("Invalid input")
+          alert("Invalid input")
+        }
+    }
+
+    const postFilterClientsRequest = useCallback(
+        (e) => {
+            e.preventDefault();
+
+            const filterParams = {filterName, filterSurname, filterEmail, filterRole}
+            sessionStorage.setItem("filterParams", JSON.stringify(filterParams));
+
+            sendFilterClientsRequest(filterParams).then(
+                (response) => {
+                    console.log(response);
+                    setClients(!!response ? response.data : []);
+                    
+                }, (error) => {
+                  console.log(error);
+                }
+            );
+        }, [filterName, filterSurname, filterEmail, filterRole]
+    )
+
     return <>
             <center><h3>Clients</h3></center>
             <br/>
-
-            {/* TODO search form */}
-
+            <div className="borderedBlock">
+                {/* TODO search form */}
+                <LabeledInput value={filterName} label="Name" inputName="name" placeholder="Type client name" required onChangeFunc={setFilterName}/>
+                <LabeledInput value={filterSurname} label="Last name" inputName="lastname" placeholder="Type client lastname" required onChangeFunc={setFilterSurname}/>
+                <LabeledInput value={filterEmail} label="Email" inputName="email" placeholder="Type client email" required onChangeFunc={setFilterEmail}/>
+                <LabeledInput value={filterRole} label="Role" inputName="role" placeholder="Type client role" required onChangeFunc={setFilterRole}/>
+                
+                <Row className='mt-2'>
+                        <Col sm={2}/>
+                        <Col sm={4} align='center'>
+                            <Button className='formButton' onClick={filterButtonPressed}>
+                                Filter
+                            </Button>
+                        </Col>
+                        <Col sm={4} align='center'>
+                            <Button className='formButton' onClick={resetButtonPressed}>
+                                Reset
+                            </Button>
+                        </Col>
+                        <Col sm={2}/>
+                  </Row> 
+            </div>
+            <br/>
             <Row>
                 <Col sm="3">
                     Name
--- security-front/src/services/api/UserApi.js
+++ security-front/src/services/api/UserApi.js
@@ -20,3 +20,23 @@ export async function getAllClients(){
         return err.message
     }
   }
+
+export async function postClientFilterRequest(){
+    try {
+        const responseData = await getApiCall().get('/user/all-clients');
+        return responseData;
+    } catch (err) {
+        console.log(err.message);
+        return err.message
+    }
+  }
+
+  export async function sendFilterClientsRequest(filterParams){
+    try {
+        const responseData = await getApiCall().post('/user/filter-clients', filterParams);
+        return responseData;
+    } catch (err) {
+        console.log(err.message);
+        return err.message
+    }
+  }
